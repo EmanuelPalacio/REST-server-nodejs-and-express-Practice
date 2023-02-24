5391f7d4431ebc511692ca64a12b5233a9a80d44
Emanuel Palacio <emanuelpalacio.dev@gmail.com>
2023-02-24 18:38:04 -0300

    route sendFile

---
+++ controllers/uploads/index.js
@@ -0,0 +1,3 @@
+export { default as updateImage } from "./updateImage.js";
+export { default as upload } from "./upload.js";
+export { default as viewFile } from "./viewFile.js";
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import { param } from "express-validator";
-import updateImage from "../controllers/uploads/updateImage.js";
-import upload from "../controllers/uploads/upload.js";
 import { colecctionUpdate } from "../utils/customCheck.js";
 import { checkFile, validateToken } from "../middlewares/index.js";
 import { checkValidator, verifyDestination } from "../middlewares/index.js";
 import configStorage from "../utils/multerStorage.js";
+import { viewFile, upload, updateImage } from "../controllers/uploads/index.js";
 
 const uploadsRoutes = Router();
+
+uploadsRoutes.get(
+  "/:colecction/:id",
+  [
+    param("id", "Tiene que ser un ID de mongo valida").isMongoId(),
+    param("colecction").custom(colecctionUpdate),
+    checkValidator,
+  ],
+  verifyDestination,
+  viewFile
+);
 
 uploadsRoutes.post(
   "/singleFile",
